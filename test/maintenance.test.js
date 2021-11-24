d7c8a067c1280832c9c07bc26d32e7c1ad341d3e
Mark Warner <mark.warner@verizon.com>
2021-11-23 18:18:42 -0600

    Update maintenance test

--- test/maintenance.test.js
+++ test/maintenance.test.js
@@ -1,8 +1,9 @@
 const Maintenance = require('../src/maintenance');
 
 describe("Maintenance class", () => {
-    const testMaintenance = new Maintenance();
     test("Creates a maintenance instance", () => {
+        const name = "John";
+        const testMaintenance = new Maintenance(name);
         expect(testMaintenance).toBeDefined();
         expect(testMaintenance instanceof Maintenance).toBeTruthy();
     });
--- test/user.test.js
+++ test/user.test.js
@@ -1,9 +1,9 @@
 const User = require('../src/user');
 
 describe("User class", () => {
-    const name = "John";
-    const testUser = new User(name);
     test("Creates a user instance", () => {
+        const name = "John";
+        const testUser = new User(name);
         expect(testUser).toBeDefined();
         expect(testUser instanceof User).toBeTruthy();
     });
